8e713811bfee5856995d18d82002971a064349f1
DESKTOP-56N0KDG\Administrator <605729908@qq.com>
2023-01-31 20:29:58 +0800

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // MessageBox.Show("hello wpf!!!");
+            string userName = this.tbName.Text;
+            if (userName == "Jovan")
+                MessageBox.Show("确认通过");
+            //this.tbName.Text = "Jovan";
+        }
+    }
+}
